cd4b61f8555d378fc5132254adce457136d9de66
ddzik19 <ddzik09@gmail.com>
2023-05-31 20:19:40 +0100

    initial commit

---
+++ app/src/App.js
@@ -0,0 +1,12 @@
+import Navbar from "./components/navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <div>body</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ app/src/components/navbar.js
@@ -0,0 +1,8 @@
+//import useWindowDimensions from "../hooks/use-window-dimensions";
+
+const Navbar = () => {
+  // const { width, height } = useWindowDimensions();
+  return <div></div>;
+};
+
+export default Navbar;
