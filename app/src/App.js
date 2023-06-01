d2cb17ca7082d0ed255cf7040da42a39de01ea82
ddzik19 <ddzik09@gmail.com>
2023-06-01 14:03:36 +0100

    working on footer

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,12 @@
 import Navbar from "./components/navbar";
+import Footer from "./components/footer";
 
 function App() {
   return (
     <div>
       <Navbar />
       <div>body</div>
+      <Footer />
     </div>
   );
 }
