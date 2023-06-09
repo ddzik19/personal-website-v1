e5e9267ce8d197c04a2f241079c7317a9e8a182c
ddzik19 <ddzik09@gmail.com>
2023-06-09 13:57:38 +0100

    new navbar

--- app/src/components/main-body.js
+++ app/src/components/main-body.js
@@ -1,18 +1,11 @@
-import Navbar from "./navbar"
+import Navbar from "./navbar";
 
 const MainBody = () => {
-    return (
-        <div className="grid grid-cols-1 md:grid-cols-[90%_1fr_10%]">
-            <div className="md:px-[300px] bg-damo-darker-green">
-                {/* Content for the left div */}
-                yayayayay
-            </div>
-            <div className="">
-                {/* Content for the right div */}
-                <Navbar />
-            </div>
-        </div>
-    )
-}
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+};
 
-export default MainBody
+export default MainBody;
--- app/src/components/navbar.js
+++ app/src/components/navbar.js
@@ -1,19 +1,49 @@
-import useWindowDimensions from '../hooks/use-window-dimensions'
+import useWindowDimensions from "../hooks/use-window-dimensions";
 
 const Navbar = () => {
   const { width, height } = useWindowDimensions();
+
   return (
-    <div>
-      {width > 1280 ? (
-        <div className='flex items-center justify-center h-screen'>
-          yaya
-        </div>
-      ) : (
-        <>
-        </>
-      )}
+    <div className="flex items-center justify-between">
+      <div className="flex items-center">
+        <img src="logo.png" alt="Logo" className="h-8 w-auto mr-4" />
+      </div>
+      <div>
+        {width > 1280 && (
+          <ul className="flex">
+            <li className="px-10 py-5 capitalize">
+              <a href="#">Test</a>
+            </li>
+            <li className="px-10 py-5 capitalize">
+              <a href="#">Test</a>
+            </li>
+            <li className="px-10 py-5 capitalize">
+              <a href="#">Test</a>
+            </li>
+            <li className="px-10 py-5 capitalize">
+              <a href="#">Test</a>
+            </li>
+          </ul>
+        )}
+        {width <= 1280 && (
+          <ul className="flex">
+            <li className="px-10 py-5 capitalize">
+              <a href="#">Test</a>
+            </li>
+            <li className="px-10 py-5 capitalize">
+              <a href="#">Test</a>
+            </li>
+            <li className="px-10 py-5 capitalize">
+              <a href="#">Test</a>
+            </li>
+            <li className="px-10 py-5 capitalize">
+              <a href="#">Test</a>
+            </li>
+          </ul>
+        )}
+      </div>
     </div>
-  )
+  );
 };
 
 export default Navbar;
