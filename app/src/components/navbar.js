4e4ac074ac4db67f77b16c728426ec1c5d96d6b1
ddzik19 <ddzik09@gmail.com>
2023-06-12 08:59:44 +0100

    Update navbar.js
    we now can place our emails and numbers above the navbar

--- app/src/components/navbar.js
+++ app/src/components/navbar.js
@@ -4,26 +4,35 @@ const Navbar = () => {
   const { width, height } = useWindowDimensions();
 
   return (
-    <div className="flex items-center justify-between">
-      <div className="flex items-center">
-        <img src="logo.png" alt="Logo" className="h-8 w-auto mr-4" />
-      </div>
-      <div>
+    <>
+      {width > 1280 && (
+        <div className="flex justify-end h-2 py-2">
+          <span id="span" className="mr-4">
+            <a href="#">info@d2.ie</a>
+          </span>
+        </div>
+      )}
+      <div className="flex items-center justify-between">
+        <div className="flex items-center">
+          <img src="logo.png" alt="Logo" className="h-8 w-auto mr-4" />
+        </div>
         {width > 1280 && (
-          <ul className="flex">
-            <li className="px-10 py-5 capitalize">
-              <a href="#">Test</a>
-            </li>
-            <li className="px-10 py-5 capitalize">
-              <a href="#">Test</a>
-            </li>
-            <li className="px-10 py-5 capitalize">
-              <a href="#">Test</a>
-            </li>
-            <li className="px-10 py-5 capitalize">
-              <a href="#">Test</a>
-            </li>
-          </ul>
+          <div className="flex items-center">
+            <ul className="flex">
+              <li className="px-10 py-5 capitalize">
+                <a href="#">Test</a>
+              </li>
+              <li className="px-10 py-5 capitalize">
+                <a href="#">Test</a>
+              </li>
+              <li className="px-10 py-5 capitalize">
+                <a href="#">Test</a>
+              </li>
+              <li className="px-10 py-5 capitalize">
+                <a href="#">Test</a>
+              </li>
+            </ul>
+          </div>
         )}
         {width <= 1280 && (
           <ul className="flex">
@@ -42,7 +51,7 @@ const Navbar = () => {
           </ul>
         )}
       </div>
-    </div>
+    </>
   );
 };
 
