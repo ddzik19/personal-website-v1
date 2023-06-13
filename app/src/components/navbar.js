c07f43dd2928eb8ba8a5e63e3ae931baa03d31b6
ddzik19 <ddzik09@gmail.com>
2023-06-13 13:53:22 +0100

    dropdown
    added a dropdown to our navbar.

--- app/src/components/navbar.js
+++ app/src/components/navbar.js
@@ -1,18 +1,25 @@
+import React, { useState } from "react";
 import useWindowDimensions from "../hooks/use-window-dimensions";
+import { MdDesignServices } from "react-icons/md";
 
 const Navbar = () => {
   const { width, height } = useWindowDimensions();
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!isDropdownOpen);
+  };
 
   return (
     <>
       {width > 1280 && (
-        <div className="flex justify-end h-2 py-2">
+        <div className="flex justify-end h-2 py-2 pr-10">
           <span id="span" className="mr-4">
             <a href="#">info@d2.ie</a>
           </span>
         </div>
       )}
-      <div className="flex items-center justify-between">
+      <div className="flex items-start justify-between pr-10">
         <div className="flex items-center">
           <img src="logo.png" alt="Logo" className="h-8 w-auto mr-4" />
         </div>
@@ -20,16 +27,69 @@ const Navbar = () => {
           <div className="flex items-center">
             <ul className="flex">
               <li className="px-10 py-5 capitalize">
-                <a href="#">Test</a>
+                <a href="#" className="hover:text-damo-primary font-semibold">
+                  About Us
+                </a>
               </li>
               <li className="px-10 py-5 capitalize">
-                <a href="#">Test</a>
+                <div className="relative">
+                  <button
+                    id="dropdownHoverButton"
+                    data-dropdown-toggle="dropdownHover"
+                    data-dropdown-trigger="hover"
+                    className="hover:text-damo-primary font-semibold flex"
+                    type="button"
+                    onClick={toggleDropdown}
+                  >
+                    Services{" "}
+                    <svg
+                      className="w-4 h-4 ml-2 mt-1"
+                      aria-hidden="true"
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M19 9l-7 7-7-7"
+                      ></path>
+                    </svg>
+                  </button>
+                  <div
+                    id="dropdownHover"
+                    className={`z-10 ${
+                      isDropdownOpen ? "" : "hidden"
+                    } absolute top-0 right-0 mt-12`}
+                    style={{ right: "-60px" }} // Adjust the value as needed
+                  >
+                    <ul
+                      className="py-2 bg-white rounded shadow w-40"
+                      aria-labelledby="dropdownHoverButton"
+                    >
+                      <li className="py-2 px-4 capitalize">
+                        <a
+                          href="#"
+                          className="hover:text-damo-primary font-semibold flex items-center"
+                        >
+                          <MdDesignServices className="mr-2" /> Web Design
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
               </li>
               <li className="px-10 py-5 capitalize">
-                <a href="#">Test</a>
+                <a href="#" className="hover:text-damo-primary font-semibold">
+                  Portfolio
+                </a>
               </li>
               <li className="px-10 py-5 capitalize">
-                <a href="#">Test</a>
+                <a href="#" className="hover:text-damo-primary font-semibold">
+                  Contact Us
+                </a>
               </li>
             </ul>
           </div>
