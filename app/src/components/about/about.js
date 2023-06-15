4ce57bfec134c65e7619ba422067aec8fabb938d
ddzik19 <ddzik09@gmail.com>
2023-06-15 21:27:59 +0100

    small push
    moving to imac,
    
    added responsive design to about section

--- app/src/components/about/about.js
+++ app/src/components/about/about.js
@@ -1,9 +1,9 @@
 const About = () => {
-  return(
-    <div className="flex justify-center items-center flex-col">
-      test
+  return (
+    <div className="flex justify-center items-center flex-col bg-[#EDF6FF]">
+      <div className="w-[450px] mg:w-[700px] lg:w-[850px] text-left">yaya</div>
     </div>
-  )
-}
+  );
+};
 
 export default About;
