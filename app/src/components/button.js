a0faf5367574e5133049f09175cfd42d43d82379
ddzik19 <ddzik09@gmail.com>
2023-06-15 19:46:35 +0100

    small fix
    navbar was transparent because bg-white doesnt work.

--- app/src/components/button.js
+++ app/src/components/button.js
@@ -1,6 +1,8 @@
 export const Button = ({ children, className }) => {
   return (
-    <button className={`capitalize border-[1px] boder-black font-semibold px-[30px] py-[10px] text-center rounded-lg ${className} hover:bg-damo-primary hover:text-[#ffffff]`}>
+    <button
+      className={`capitalize border-[1px] border-black font-semibold px-[30px] py-[10px] text-center rounded-lg ${className} hover:bg-damo-primary hover:text-[#ffffff]`}
+    >
       {children}
     </button>
   );
